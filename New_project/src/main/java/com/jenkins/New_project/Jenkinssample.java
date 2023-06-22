3c0b2ca160b5b5cb61b0506bb997d3c6794c2e18
sumayyaks96 <sumayyaks96@gmail.com>
2023-06-22 14:54:12 +0530

    jenkins

---
+++ New_project/src/main/java/com/jenkins/New_project/Jenkinssample.java
@@ -0,0 +1,10 @@
+package com.jenkins.New_project;
+
+public class Jenkinssample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("Sample output");
+	}
+
+}
